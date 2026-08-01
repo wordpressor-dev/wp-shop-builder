4f5cc56ffc302b79844f0d8edbf05e8f56bdfd87
wordpressor-dev <serghei.colos@gmail.com>
2026-08-01 16:32:13 +0300

    feat: add release application service (#22)

---
+++ packages/Release/Service/ReleaseService.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WPShop\Release\Service;
+
+use WPShop\Release\Contracts\ReleaseRepositoryInterface;
+use WPShop\Release\Contracts\ReleaseServiceInterface;
+use WPShop\Release\Exception\ReleaseNotFound;
+use WPShop\Release\Release;
+use WPShop\Release\ReleaseCreateData;
+use WPShop\Release\ReleaseUpdateData;
+
+final readonly class ReleaseService implements
+    ReleaseServiceInterface
+{
+    public function __construct(
+        private ReleaseRepositoryInterface $repository
+    ) {
+    }
+
+    public function create(
+        ReleaseCreateData $data
+    ): Release {
+        return $this->repository->create($data);
+    }
+
+    public function update(
+        int $id,
+        ReleaseUpdateData $data
+    ): Release {
+        $release = $this->repository->update(
+            $id,
+            $data
+        );
+
+        if ($release === null) {
+            throw ReleaseNotFound::byId($id);
+        }
+
+        return $release;
+    }
+
+    public function getById(int $id): Release
+    {
+        $release = $this->repository->findById($id);
+
+        if ($release === null) {
+            throw ReleaseNotFound::byId($id);
+        }
+
+        return $release;
+    }
+
+    public function getByBlueprintAndVersion(
+        int $blueprintId,
+        string $version
+    ): Release {
+        $release = $this->repository
+            ->findByBlueprintAndVersion(
+                $blueprintId,
+                $version
+            );
+
+        if ($release === null) {
+            throw ReleaseNotFound
+                ::byBlueprintAndVersion(
+                    $blueprintId,
+                    $version
+                );
+        }
+
+        return $release;
+    }
+}
