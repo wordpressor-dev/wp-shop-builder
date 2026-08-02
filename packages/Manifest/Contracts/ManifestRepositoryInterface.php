81473f75b2965b1a039b8c186e37f0048ebe3a63
wordpressor-dev <serghei.colos@gmail.com>
2026-08-02 16:55:56 +0300

    feat: add manifest update persistence (#40)

--- packages/Manifest/Contracts/ManifestRepositoryInterface.php
+++ packages/Manifest/Contracts/ManifestRepositoryInterface.php
@@ -6,12 +6,18 @@ namespace WPShop\Manifest\Contracts;
 
 use WPShop\Manifest\Manifest;
 use WPShop\Manifest\ManifestCreateData;
+use WPShop\Manifest\ManifestUpdateData;
 
 interface ManifestRepositoryInterface
 {
     public function create(
         ManifestCreateData $data
     ): Manifest;
+
+    public function update(
+        int $id,
+        ManifestUpdateData $data
+    ): ?Manifest;
 
     public function findById(
         int $id
