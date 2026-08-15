7d4f91229412961d3ba650e9c22e9d8f29c67e32
wordpressor-dev <serghei.colos@gmail.com>
2026-08-15 12:30:44 +0300

    feat: add Envato item data object

---
+++ apps/Plugin/ProductManager/Envato/EnvatoItem.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WPShop\App\Plugin\ProductManager\Envato;
+
+final readonly class EnvatoItem
+{
+    /**
+     * @param list<string> $tags
+     */
+    public function __construct(
+        public int $itemId,
+        public string $baseTitle,
+        public string $productSlug,
+        public string $version,
+        public string $updatedDate,
+        public string $developer,
+        public string $salesPage,
+        public int $sales,
+        public ?string $publishedAt,
+        public array $tags,
+        public string $skuFilename
+    ) {
+    }
+}
