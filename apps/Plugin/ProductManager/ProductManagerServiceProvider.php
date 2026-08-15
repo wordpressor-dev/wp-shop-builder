694f745bc85244dc950b079da0ceb71224782b33
wordpressor-dev <serghei.colos@gmail.com>
2026-08-15 12:33:50 +0300

    feat: register Product Manager Envato mapping and tag services

--- apps/Plugin/ProductManager/ProductManagerServiceProvider.php
+++ apps/Plugin/ProductManager/ProductManagerServiceProvider.php
@@ -6,6 +6,10 @@ namespace WPShop\App\Plugin\ProductManager;
 
 use LogicException;
 use WPShop\App\Plugin\Admin\ProductManagerPage;
+use WPShop\App\Plugin\ProductManager\Envato\EnvatoItemMapper;
+use WPShop\App\Plugin\ProductManager\Tags\Contracts\CatalogTagRepositoryInterface;
+use WPShop\App\Plugin\ProductManager\Tags\ExistingTagSelector;
+use WPShop\App\Plugin\ProductManager\Tags\WordPressCatalogTagRepository;
 use WPShop\Core\Container\ContainerInterface;
 use WPShop\Core\Contracts\KernelInterface;
 use WPShop\Core\Provider\AbstractServiceProvider;
@@ -25,9 +29,29 @@ final class ProductManagerServiceProvider extends AbstractServiceProvider
             );
         }
 
+        $mapper = new EnvatoItemMapper();
+        $tagRepository = new WordPressCatalogTagRepository();
+        $tagSelector = new ExistingTagSelector($tagRepository);
         $page = new ProductManagerPage();
+
         $registry->addSubmenu($page);
 
+        $this->container->set(
+            EnvatoItemMapper::class,
+            $mapper
+        );
+        $this->container->set(
+            CatalogTagRepositoryInterface::class,
+            $tagRepository
+        );
+        $this->container->set(
+            WordPressCatalogTagRepository::class,
+            $tagRepository
+        );
+        $this->container->set(
+            ExistingTagSelector::class,
+            $tagSelector
+        );
         $this->container->set(
             ProductManagerPage::class,
             $page
