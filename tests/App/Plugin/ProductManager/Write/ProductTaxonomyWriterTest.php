d83c2cd098f4646391d9980bc0e5e7257c0010f8
wordpressor-dev <serghei.colos@gmail.com>
2026-08-28 12:44:17 +0300

    style: format template taxonomy test closure

--- tests/App/Plugin/ProductManager/Write/ProductTaxonomyWriterTest.php
+++ tests/App/Plugin/ProductManager/Write/ProductTaxonomyWriterTest.php
@@ -110,7 +110,10 @@ final class ProductTaxonomyWriterTest extends TestCase
             static function (
                 string $name,
                 mixed ...$arguments
-            ) use (&$setTerms, $terms): mixed {
+            ) use (
+                &$setTerms,
+                $terms
+            ): mixed {
                 if ($name === 'sanitize_title') {
                     return strtolower(str_replace(' ', '-', (string) $arguments[0]));
                 }
