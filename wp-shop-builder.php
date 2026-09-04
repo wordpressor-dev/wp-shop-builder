5d3be65f00bf1958d2fe0dfd0e61867eb9807d96
wordpressor-dev <serghei.colos@gmail.com>
2026-09-04 22:54:57 +0300

    chore: bump plugin version for INBOX identity fix

--- wp-shop-builder.php
+++ wp-shop-builder.php
@@ -4,7 +4,7 @@
  * Plugin Name: WP Shop Builder
  * Plugin URI: https://wp-shop.org
  * Description: Digital Product Platform for WordPress.
- * Version: 0.2.2
+ * Version: 0.2.3
  * Requires at least: 6.8
  * Requires PHP: 8.3
  * Requires Plugins: woocommerce
