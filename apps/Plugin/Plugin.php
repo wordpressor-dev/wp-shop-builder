3d001eb89eee534cf6245e5d14eaae5100c4018a
wordpressor-dev <serghei.colos@gmail.com>
2026-09-05 12:11:46 +0300

    chore: align runtime version to 0.2.6

--- apps/Plugin/Plugin.php
+++ apps/Plugin/Plugin.php
@@ -17,7 +17,7 @@ use WPShop\WordPress\Bootstrap\Bootstrap as WordPressBootstrap;
 
 final readonly class Plugin
 {
-    public const VERSION = '0.2.5';
+    public const VERSION = '0.2.6';
 
     /**
      * @param null|Closure(
