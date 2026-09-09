ea70841a72be1573a30d1d23be08bfdadf6379d7
wordpressor-dev <serghei.colos@gmail.com>
2026-09-09 09:49:30 +0300

    Test Probiz ThemeForest page-tag extraction

--- tests/App/Plugin/ProductManager/Editorial/TemplateKitSalesPageEditorialTest.php
+++ tests/App/Plugin/ProductManager/Editorial/TemplateKitSalesPageEditorialTest.php
@@ -55,6 +55,17 @@ final class TemplateKitSalesPageEditorialTest extends TestCase
 This kit has been optimized for use with the free "Hello Elementor" theme.</p>
 <p>Images This Template Kit uses demo images from Envato Elements.
 You will need to license these images from Envato Elements to use them.</p>
+<div class="tags">
+<a href="https://themeforest.net/search/agency">agency</a>
+<a href="/search/business">business</a>
+<a href="https://themeforest.net/search/company">company</a>
+<a href="https://themeforest.net/search/consulting">consulting</a>
+<a href="https://themeforest.net/search/corporate">corporate</a>
+<a href="https://themeforest.net/search/finance">finance</a>
+<a href="https://themeforest.net/search/landingpage">landingpage</a>
+<a href="https://themeforest.net/search/marketing">marketing</a>
+<a href="https://themeforest.net/search/startup">startup</a>
+</div>
 </div>
 HTML;
 
@@ -69,6 +80,20 @@ HTML;
         self::assertTrue($facts['demoImagesLicense']);
         self::assertContains('Homepage', $facts['templates']);
         self::assertContains('RomethemeForm', $facts['requiredPlugins']);
+        self::assertSame(
+            [
+                'agency',
+                'business',
+                'company',
+                'consulting',
+                'corporate',
+                'finance',
+                'landingpage',
+                'marketing',
+                'startup',
+            ],
+            $facts['tags']
+        );
 
         $base = (new ProductEditorialDraftBuilder())->build(
             'Probiz – Business Consulting Elementor Template Kit',
