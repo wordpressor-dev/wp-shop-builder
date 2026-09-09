64df154c9591def5ad74d0e02ebcff5a715bb3b7
wordpressor-dev <serghei.colos@gmail.com>
2026-09-09 10:41:16 +0300

    Preserve HTML text boundaries in Template Kit facts

--- apps/Plugin/ProductManager/Editorial/EnvatoTemplateKitSalesPageExtractor.php
+++ apps/Plugin/ProductManager/Editorial/EnvatoTemplateKitSalesPageExtractor.php
@@ -208,8 +208,13 @@ final class EnvatoTemplateKitSalesPageExtractor
             ' ',
             $html
         );
+        $html = (string) preg_replace(
+            '/<[^>]+>/u',
+            ' ',
+            $html
+        );
         $text = html_entity_decode(
-            strip_tags($html),
+            $html,
             ENT_QUOTES | ENT_HTML5,
             'UTF-8'
         );
